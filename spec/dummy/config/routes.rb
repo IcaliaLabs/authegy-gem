2bd1402d4614da42a73e0cfd6eafa3d1dbd38bde
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-03-06 12:48:21 -0600

    Added router mapped method 'authegy_routes'

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   resources :group_posts
   resources :user_groups
 
-  devise_for :users,
-             path: '/',
-             path_names: { sign_in: 'sign-in', sign_out: 'sign-out' }
+  authegy_routes
 
   # For details on the DSL available within this file,
   # see http://guides.rubyonrails.org/routing.html
