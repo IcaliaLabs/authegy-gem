313097cbc0abd63f866d4e7e5d08329713623bd5
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-03-06 14:05:30 -0600

    Fixed class_name on User reference to Person

--- lib/authegy/models/user.rb
+++ lib/authegy/models/user.rb
@@ -15,7 +15,11 @@ module Authegy
     devise :database_authenticatable_with_person_email,
            :validatable_with_person_email
 
-    belongs_to :person, inverse_of: :user, foreign_key: :id
+    belongs_to :person,
+               class_name: '::Person',
+               inverse_of: :user,
+               foreign_key: :id
+
     delegate :email, :email=, :name, to: :person, allow_nil: true
 
     delegate :assigned_roles, :assign_role, :has_role?, :has_any_role?,
