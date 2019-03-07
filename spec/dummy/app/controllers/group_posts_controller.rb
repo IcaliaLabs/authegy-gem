293da7aa440e1661cd90f1ff266d62b437d14bec
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-03-07 16:55:29 -0600

    Updated dummy app with access authorization DSL

--- spec/dummy/app/controllers/group_posts_controller.rb
+++ spec/dummy/app/controllers/group_posts_controller.rb
@@ -1,9 +1,26 @@
 class GroupPostsController < ApplicationController
-  before_action :set_group_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_group_post, only: %i[show edit update destroy]
+
+  # NOTE: All Authegy authorization processes are bypassed for requests from:
+  # - People with an unscoped 'Administrator' role (i.e. without a resource)
+  # - OAuth2 Apps with a trust level of 0 (the most trustworthy), authenticated
+  #   via "Client Credentials"
+
+  # The first guard on our authorization process, acts upon the @user_groups and
+  # @user_group variables, hiding records that fail to match the authorization
+  # criteria from the results:
+  authorize_access_for :authors, of: 'group_post', to: GroupPost
+
+  authorize_access_for :owners, :moderators, :members,
+                       of: 'group_post.group',
+                       to: GroupPost
+
+  # Would this be possible??
+  # authorize_access_to GroupPost, for: :moderators, :owners, :members, of: '.group'
 
   # GET /group_posts
   def index
-    @group_posts = GroupPost.all
+    @posts = authorized_group_posts.where(group_id: params[:group_id])
   end
 
   # GET /group_posts/1
@@ -12,7 +29,7 @@ class GroupPostsController < ApplicationController
 
   # GET /group_posts/new
   def new
-    @group_post = GroupPost.new
+    @post = GroupPost.new
   end
 
   # GET /group_posts/1/edit
@@ -21,10 +38,10 @@ class GroupPostsController < ApplicationController
 
   # POST /group_posts
   def create
-    @group_post = GroupPost.new(group_post_params)
+    @post = GroupPost.new(group_post_params)
 
-    if @group_post.save
-      redirect_to @group_post, notice: 'Group post was successfully created.'
+    if @post.save
+      redirect_to @post, notice: 'Group post was successfully created.'
     else
       render :new
     end
@@ -32,8 +49,8 @@ class GroupPostsController < ApplicationController
 
   # PATCH/PUT /group_posts/1
   def update
-    if @group_post.update(group_post_params)
-      redirect_to @group_post, notice: 'Group post was successfully updated.'
+    if @post.update(group_post_params)
+      redirect_to @post, notice: 'Group post was successfully updated.'
     else
       render :edit
     end
@@ -41,18 +58,19 @@ class GroupPostsController < ApplicationController
 
   # DELETE /group_posts/1
   def destroy
-    @group_post.destroy
+    @post.destroy
     redirect_to group_posts_url, notice: 'Group post was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group_post
-      @group_post = GroupPost.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def group_post_params
-      params.require(:group_post).permit(:group_id, :author_id, :title, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group_post
+    @post = authorized_group_posts.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def group_post_params
+    params.require(:group_post).permit(:group_id, :author_id, :title, :body)
+  end
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   authegy_routes
   root to: 'user_groups#index'
 
-  resources :group_posts
-  resources :user_groups
+  resources :groups, controller: :user_groups do
+    resources :posts, controller: :group_posts
+  end
 
   # For details on the DSL available within this file,
   # see http://guides.rubyonrails.org/routing.html
