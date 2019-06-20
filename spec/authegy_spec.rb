ea0fa65480165c17199ca91ff3d7b6afe8f2599b
Alan Rocha <alan.rocha@udem.edu>
2019-06-20 15:13:28 -0500

    Added that a test can copy and remove the sample_app from the tmp folder

--- spec/authegy_spec.rb
+++ spec/authegy_spec.rb
@@ -1,9 +1,19 @@
-RSpec.describe Authegy do
-  it 'has a version number' do
-    expect(Authegy::VERSION).not_to be nil
+require 'spec_helper'
+
+RSpec.describe 'authegy:install', type: :generator do
+  
+  sample_app_ref =  File.expand_path('../fixtures/sample_app', __FILE__) 
+  sample_app = File.expand_path('../tmp/sample_app', __FILE__)
+
+  before(:all) do
+    FileUtils.cp_r sample_app_ref, sample_app
   end
 
-  it 'does something useful' do
-    expect(false).to eq true
+  after(:all) do
+    FileUtils.rm_rf sample_app
+  end
+  
+  it "should do something" do
+    expect(true).to eq true
   end
 end
