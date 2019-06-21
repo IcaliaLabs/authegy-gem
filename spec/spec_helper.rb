d8eeb09b99356ddbc07b8f5e74d428969d774fb0
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-06-21 13:00:42 -0500

    Added byebug to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'bundler/setup'
 require 'authegy'
 require 'fileutils'
+require 'byebug'
 require 'pry-rails'
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
