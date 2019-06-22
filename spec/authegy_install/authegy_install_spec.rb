40d2f949db4a30214410fe838897e965c3bf9dea
Alan Rocha <alan.rocha@udem.edu>
2019-06-22 00:55:42 -0500

    Added tests that check if all generated files have the expected content

--- spec/authegy_install/authegy_install_spec.rb
+++ spec/authegy_install/authegy_install_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'authegy:install', type: :generator do
 
   around do |example|
     # Setup
-    example_app_source_path = File.expand_path('../fixtures/sample_app', __FILE__)
+    example_app_source_path = File.expand_path('../../fixtures/sample_app', __FILE__)
     example_app_path = File.expand_path('../tmp/', __FILE__)
     original_path = Dir.pwd
 
@@ -26,9 +26,20 @@ RSpec.describe 'authegy:install', type: :generator do
       expect(File).to exist 'app/models/user.rb'
       expect(File).to exist 'app/models/person.rb'
       expect(File).to exist 'app/models/role.rb'
-      expect(File).to exist 'app/models/user.rb'
       expect(File).to exist 'app/models/role_assignment.rb'
       expect(File).to exist "db/migrate/#{migration_file}"
     end
+
+    it 'all generated files should have the expected content' do
+      templates_path = "../../../lib/generators/authegy/templates/"
+      run_generator 'authegy:install'
+      expect(FileUtils.compare_file(File.expand_path("app/models/user.rb"), File.expand_path("#{templates_path}user_model.rb"))).to be_truthy
+      expect(FileUtils.compare_file(File.expand_path("app/models/person.rb"), File.expand_path("#{templates_path}person_model.rb"))).to be_truthy
+      expect(FileUtils.compare_file(File.expand_path("app/models/role.rb"), File.expand_path("#{templates_path}role_model.rb"))).to be_truthy
+      expect(FileUtils.compare_file(File.expand_path("app/models/role_assignment.rb"), File.expand_path("#{templates_path}role_assignment_model.rb"))).to be_truthy
+      expect(File.read(File.expand_path("db/migrate/#{migration_file}"))).to eq(render_erb_file("#{templates_path}models_migration.erb"))
+    end
+
   end
 end
+    
--- spec/authegy_install/authegy_install_spec_helper.rb
+++ spec/authegy_install/authegy_install_spec_helper.rb
@@ -7,4 +7,10 @@ end
 
 def migration_file
     Dir.entries(File.expand_path("../tmp/db/migrate", __FILE__)).each { |entry| entry.scan(/\d/).join.to_i }.max
-  end
+end
+
+def render_erb_file(template)
+    migration_version = "[5.2]"
+    render = ERB.new(File.read(template))
+    render.result(binding)
+end
