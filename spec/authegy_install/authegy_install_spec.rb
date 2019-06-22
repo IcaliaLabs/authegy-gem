ce3792556e6361aa905869c66b6b5044a074c0de
Alan Rocha <alan.rocha@udem.edu>
2019-06-21 22:23:26 -0500

    Added tests of existence of generated authegy files

--- spec/authegy_install/authegy_install_spec.rb
+++ spec/authegy_install/authegy_install_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'authegy:install', type: :generator do
 
   around do |example|
     # Setup
-    example_app_source_path = File.expand_path('../../fixtures/sample_app', __FILE__)
+    example_app_source_path = File.expand_path('../fixtures/sample_app', __FILE__)
     example_app_path = File.expand_path('../tmp/', __FILE__)
     original_path = Dir.pwd
 
@@ -18,10 +18,17 @@ RSpec.describe 'authegy:install', type: :generator do
     Dir.chdir original_path
     FileUtils.rm_rf example_app_path
   end
+
+  context 'runs the devise & authegy installation' do 
   
-  it 'runs the devise & authegy installation' do
-    run_generator 'authegy:install'
-    expect(File).to exist 'app/models/user.rb'
-    expect(File).to exist 'app/models/person.rb'
+    it 'should exist all authegy files' do
+      run_generator 'authegy:install'
+      expect(File).to exist 'app/models/user.rb'
+      expect(File).to exist 'app/models/person.rb'
+      expect(File).to exist 'app/models/role.rb'
+      expect(File).to exist 'app/models/user.rb'
+      expect(File).to exist 'app/models/role_assignment.rb'
+      expect(File).to exist "db/migrate/#{migration_file}"
+    end
   end
 end
--- spec/authegy_install/authegy_install_spec_helper.rb
+++ spec/authegy_install/authegy_install_spec_helper.rb
@@ -4,3 +4,7 @@ require 'fileutils'
 def run_generator(generator_name)
     system "rails g #{generator_name}"
 end
+
+def migration_file
+    Dir.entries(File.expand_path("../tmp/db/migrate", __FILE__)).each { |entry| entry.scan(/\d/).join.to_i }.max
+  end
