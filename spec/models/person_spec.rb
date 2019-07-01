ad5ba9cd2394862d8f3bb3018642f4d69c09d020
Alan Rocha <alan.rocha@udem.edu>
2019-06-30 21:37:11 -0500

    Added test that check if people table exists

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -3,12 +3,16 @@
 require "rails_helper"
 
 RSpec.describe Authegy::Person, type: :model do
+
+  it "should be exist a table called 'people'" do
+    expect(ActiveRecord::Base.connection.table_exists? 'people').to be_truthy
+  end
+
   it { should have_one(:user).inverse_of(:person) }
+
   it { is_expected.to validate_uniqueness_of(:email) }
 
-  # it 'has a user' do
-  #   expect(subject).to respond_to :user
-  # end
+  
 
 
 end
