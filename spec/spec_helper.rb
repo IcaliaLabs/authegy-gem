94f5a94a1ddcffd580bc99704f479fb2d5d7c384
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-07-02 13:24:25 -0500

    Use 'rails_helper' on rspec (#11)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 require 'authegy'
 require 'fileutils'
+require 'shoulda-matchers'
+require 'active_record'
 require 'byebug'
 require 'pry-rails'
 
@@ -13,5 +15,16 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.order = :random
+
+  Kernel.srand config.seed
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
   end
 end
