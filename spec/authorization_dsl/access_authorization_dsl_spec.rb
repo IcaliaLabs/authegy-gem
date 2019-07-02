e59a7cbc608862f5fcbcdd45b95a490a0b43a0a6
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-07-02 17:54:09 -0500

    Added more tests...

--- spec/authorization_dsl/access_authorization_dsl_spec.rb
+++ spec/authorization_dsl/access_authorization_dsl_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Authorization DSL' do
-  let(:described_class) do
+  let(:described_class) do |example|
     Class.new(ApplicationController) do
       include Authegy::Authorization::Helpers
     end
+  end
+
+  it 'responds to .access_authorization with a ruleset' do
+    expect(described_class).to respond_to :access_authorization
+    expect(described_class.access_authorization)
+      .to be_kind_of Authegy::Authorization::AccessRuleSet
   end
 
   it 'responds to .authorize_access_for' do
@@ -27,6 +33,29 @@ RSpec.describe 'Authorization DSL' do
       
       expect(described_class._process_action_callbacks)
         .to include an_object_having_attributes filter: :run_resource_authorization
+    end
+
+    it 'makes an authorized scope helper available to the class' do
+      expect(described_class.new).not_to respond_to :authorized_user_groups
+      subject
+      expect(described_class.new).to respond_to :authorized_user_groups
+    end
+    
+    context 'with no restrictable class' do
+      let(:example_params) { [:authors, of: 'group_post'] }
+
+      it 'does not add any new rules to the access_authorization object' do
+        expect { subject }.not_to change { described_class.access_authorization.count }
+      end
+  
+      it 'does not add the "run_resource_authorization" callback' do
+        expect do
+          subject
+        end.not_to change { described_class._process_action_callbacks.count }
+        
+        expect(described_class._process_action_callbacks)
+          .not_to include an_object_having_attributes filter: :run_resource_authorization
+      end
     end
   end
 end
