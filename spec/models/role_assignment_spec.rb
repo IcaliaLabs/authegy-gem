9aa48f2a726408cd2b1414024360bf7c041ef149
Alan Rocha <alan.rocha@udem.edu>
2019-07-03 17:44:07 -0500

    Added associations test

--- spec/models/role_assignment_spec.rb
+++ spec/models/role_assignment_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe Authegy::RoleAssignment, type: :model do
       expect(described_class.table_name).to eq 'role_assignments'
     end
   end
+
+  describe 'associations' do
+    it 'belongs to "actor"' do
+      is_expected.to belong_to(:actor)
+    end
+
+    it 'belongs to "role"' do
+        is_expected.to belong_to(:role)
+    end
+
+    it 'belongs to "resource"' do
+        is_expected.to belong_to(:resource).optional
+    end
+  end
 end
