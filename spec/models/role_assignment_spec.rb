53502094af65e096777db5aac7abf4e2a67b796f
Alan Rocha <alan.rocha@udem.edu>
2019-07-03 17:31:08 -0500

    The test check that RoleAssigmen use the role_assignment name

--- spec/models/role_assignment_spec.rb
+++ spec/models/role_assignment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Authegy::RoleAssignment, type: :model do
+  let(:described_class) do |example|
+    ::RoleAssignment
+  end
 
+  describe 'persistence' do
+    it "uses the 'role_assignments' table" do
+      expect(described_class.table_name).to eq 'role_assignments'
+    end
+  end
 end
