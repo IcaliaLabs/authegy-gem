182c324f5a95c2b9c3c2067cd29b534bae9ae8b3
Alan Rocha <alan.rocha@udem.edu>
2019-07-02 19:36:35 -0500

    Added .assign_role test

--- spec/models/authegy_authorizable_examples.rb
+++ spec/models/authegy_authorizable_examples.rb
@@ -4,6 +4,14 @@ require 'rails_helper'
 
 RSpec.shared_examples 'of an Authegy::Authorizable model' do
 
+  let(:described_class) do |example|
+    ::Person
+  end
+
+  let(:example_role) { ::Role.create! name: 'example_role' }
+  let(:example_person) { ::Person.create! email: 'example@person.com' }
+  let(:example_owner) { ::Person.create! email: 'owner@person.com' }
+
   describe "Authegy::Authorizable associations" do
     it 'has many role assignments' do
       is_expected.to have_many(:role_assignments)
@@ -19,10 +27,6 @@ RSpec.shared_examples 'of an Authegy::Authorizable model' do
     end
 
     it 'assigned roles are unique' do
-      example_role = ::Role.create! name: 'example_role'
-      example_person = ::Person.create! email: 'example@person.com'
-      example_owner = ::Person.create! email: 'owner@person.com'
-
       # Let's make sure we have more than one role assigned to the user:
       [
         UserGroup.create!(name: 'example-group', owner: example_owner),
@@ -36,6 +40,28 @@ RSpec.shared_examples 'of an Authegy::Authorizable model' do
       expect(example_person.role_assignments.count).to eq 2
       expect(example_person.assigned_roles).to include example_role
       expect(example_person.assigned_roles.count).to eq 1
+    end
+
+    describe '.assign_role ' do
+      
+      context 'without a context resource' do
+
+        it 'assings a role to a person' do
+          expect(example_person.assign_role(example_role)).to eq ::RoleAssignment.first
+        end
+
+      end
+
+      context 'with a context resource' do
+
+        resource = 'admin'
+        it 'assings a role to a person' do
+          expect(example_person.assign_role(example_role, resource)).to eq ::RoleAssignment.first
+        end
+
+      end
+
+
     end
   end
 end
